4beff8e807d0c511b9ec0a60f8982694b6afb6b6
Xiaolong Wang <dragonxlwang@gmail.com>
2016-03-25 01:12:09 -0500

    tmp

---
+++ vectors/dcme.c
@@ -0,0 +1,16 @@
+//////////////////////////////////////////////////////////////////
+// Copyright (c) 2016 Xiaolong Wang All Rights Reserved.        //
+// Code for ``Dual Clustered Maximal Entropy for Many Classes'' //
+//////////////////////////////////////////////////////////////////
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../utils/util_misc.c"
+#include "../utils/util_num.c"
+#include "../utils/util_text.c"
+
+struct Model {
+  double *scr;
+}
