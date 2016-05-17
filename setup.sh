fc2ca110fd5d3cb399e3bb3bf0be5b2d6fddbf8c
Xiaolong Wang <dragonxlwang@gmail.com>
2016-05-17 17:38:44 -0500

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# setup script
+
+# download data if necessary
+mkdir -p ~/data
+if [[ ! -d ~/data/text8 ]]; then
+  pushd ~/data > /dev/null
+  curl -OL http://timan102.cs.illinois.edu/~xwang95/data/text8.gz
+  gunzip text8.gz
+  popd > /dev/null
+fi
+
+if [[ ! -d ~/data/gigaword ]]; then
+  mkdir ~/data/gigaword
+  pushd ~/data/gigaword > /dev/null
+  curl -OL http://timan102.cs.illinois.edu/~xwang95/data/gigaword/giga_nyt.txt
+  curl -OL http://timan102.cs.illinois.edu/~xwang95/data/gigaword/giga_nyt.vcb
+  popd > /dev/null
+fi
+
+
