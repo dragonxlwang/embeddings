b33fcdec683df39eeabe793d11f7e4463b1b7ad6
Xiaolong Wang <dragonxlwang@gmail.com>
2016-08-23 20:05:56 -0700

    message

--- classify/dcme.c
+++ classify/dcme.c
@@ -218,7 +218,8 @@ void DcmeDualUpdate(int zz, DcmeBookkeeping* b, heap* twh) {
       twps += b->dd[zz * C + k];
     }
     b->twps[zz] = twps;
-    LOG(2, "TWPS: %.3e\n", twps);
+    LOG(2, "TWPS: %.3e, zz: %d, blst pos: %d\n", twps, zz,
+        (b - blst) / sizeof(DcmeBookkeeping*));
   }
   if (V_MICRO_ME) {  // Q > 0 required
     NumFillZeroVec(ow, N);
