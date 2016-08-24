d911999a60b63f9417094a9533e40c38db0e0dd5
Xiaolong Wang <dragonxlwang@gmail.com>
2016-08-23 20:33:34 -0700

    message

--- classify/dcme.c
+++ classify/dcme.c
@@ -63,7 +63,7 @@ char* DcmeDualModelDebugInfoStr(DcmeBookkeeping* b) {
   sprintfc(ddis, 'g', 'k', "ENT:%.2e\u00b1%.2e", eem, ees);
   saprintf(ddis, " ");
   real tp = 0;
-  for (j = 0; j < Q; j++) tp += b->dd[b->last_updated_zz * Q + b->tw[j]];
+  for (j = 0; j < Q; j++) tp += b->dd[b->last_updated_zz * C + b->tw[j]];
   saprintf(ddis, "top probability: %.3e twps: %.3e zz: %d", tp,
            b->twps[b->last_updated_zz], b->last_updated_zz);
   if (NumIsNan(tp)) {
@@ -73,7 +73,6 @@ char* DcmeDualModelDebugInfoStr(DcmeBookkeeping* b) {
     saprintf(ddis, "%s", pb);
     free(pb);
   }
-
   /* real dd[QUP]; */
   /* for (j = 0; j < Q; j++) dd[j] = b->dd[b->last_updated_zz * Q + b->tw[j]];
    */
